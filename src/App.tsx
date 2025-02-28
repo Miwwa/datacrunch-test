4ca9a3691acc3f2fe0fb6177c092c060fcc5c215
Mikhail <eremin.ms92@gmaill.com>
2025-02-28 21:16:01 +0200

    Remove default React app assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css'
+import {
+  Button,
+  Form,
+  Group,
+  Input,
+  Label,
+  NumberField,
+  Slider,
+  SliderOutput,
+  SliderThumb,
+  SliderTrack,
+  TextField
+} from "react-aria-components";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Form</h1>
+      <div className={styles.card}>
+        <Form>
+          <TextField>
+            <Label>Name</Label>
+            <Input/>
+          </TextField>
+          <NumberField defaultValue={4} minValue={0}>
+            <Label>Size (GB)</Label>
+            <Group>
+              <Button slot="decrement">-</Button>
+              <Input/>
+              <Button slot="increment">+</Button>
+            </Group>
+          </NumberField>
+          <Slider defaultValue={30}>
+            <Label/>
+            <SliderOutput/>
+            <SliderTrack style={{height: 30}}>
+              <SliderThumb/>
+            </SliderTrack>
+          </Slider>
+          <div style={{display: 'flex', gap: 8}}>
+            <Button type="reset">Clear</Button>
+            <Button type="submit">Submit</Button>
+          </div>
+        </Form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
