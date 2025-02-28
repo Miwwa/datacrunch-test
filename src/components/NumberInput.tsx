4e8fe0bd1e4fc1cac629fe390671932f0e2397f5
Mikhail <eremin.ms92@gmaill.com>
2025-02-28 23:17:02 +0200

    Add NumberInput component and update input styling
    Introduced a new `NumberInput` component with customizable props and styling for numeric input. Updated the `TextInput` styles for improved design consistency, replacing borders with inset box shadows and adjusting sizing and focus states. These changes enhance the UI uniformity and functionality across input components.

---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,31 @@
+import {Button, Input, NumberField} from "react-aria-components";
+import styles from './NumberInput.module.css'
+
+export type NumberInputProps = {
+  value?: number
+  onChange?: (value: number) => void
+  minValue?: number
+  maxValue?: number
+  isDisabled?: boolean
+}
+
+export default function NumberInput({value, onChange, minValue, maxValue, isDisabled}: NumberInputProps) {
+  return (
+    <NumberField
+      value={value}
+      minValue={minValue}
+      maxValue={maxValue}
+      onChange={onChange}
+      className={styles.field}
+      isDisabled={isDisabled}
+    >
+      <Button slot="decrement" className={styles.button}>
+        -
+      </Button>
+      <Input className={styles.input}/>
+      <Button slot="increment" className={styles.button}>
+        +
+      </Button>
+    </NumberField>
+  )
+}
